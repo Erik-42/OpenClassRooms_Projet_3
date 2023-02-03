c3a20239906f133d4b294fb491f774d05d2fbdb2
Erik-42 <mesen.erik@gmail.com>
2023-02-03 11:19:46 +0100

    Synchro-page-login-av-token

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,14 +1,30 @@
 //localStorage.clear()
-document.getElementById("connect").onclick = function (event) {
-    document.getElementById("password").select();
+const loginForm = document.getElementById("loginForm");
+const connect = document.getElementById("connect");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-    //methode 1
-    let ID = 'admin';
-    let PWD = 'admin';
-    function compareLogin(mail, password) {
-        if (mail == ID & password == PWD) {
-            document.write('Bienvenue');
-        } else {
-            document.write('Accés Refusé');
-        }
-    }
+connect.addEventListener("click", (event) => {
+  document.getElementById("password").select();
+});
+/*email: sophie.bluel@test.tld
+password: S0phie */
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  console.log(email.value, password.value);
+  const bodyJson = JSON.stringify({
+    email: email.value,
+    password: password.value,
+  });
+  console.log(bodyJson);
+  const login = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: bodyJson,
+  });
+  const data = await login.json();
+  //todo tester token si pas token message erreur sinon sessionstorage
+  window.location.href = "./index.html";
+});
--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,47 +1,46 @@
-//localStorage.clear;
-//Récupération des fiches eventuellement stockées dans le localStorage
-let fiches = window.localStorage.getItem('fiches');
+//sessionStorage.clear;
 
-// Fiches stockées en local
+//Récupération des fiches eventuellement stockées dans le sessionStorage
+let fiches = window.sessionStorage.getItem("fiches");
+
+// Fiches stockées en session
 if (fiches === null) {
-    // Récupération des fiches depuis le fichier JSON
-    const askApi = await fetch('./js/works.json');
-    //const askApi = await fetch('http://localhost:5678/api/works');
-    fiches = await askApi.json();
+  // Récupération des fiches depuis le fichier JSON
+  //const askApi = await fetch('./js/works.json');
+  const askApi = await fetch("http://localhost:5678/api/works");
+  fiches = await askApi.json();
 
-    // Transformation des fiches en JSON
-    const valeurFiches = JSON.stringify(fiches);
-    // Stockage des informations dans le localStorage
-    window.localStorage.setItem("fiches", valeurFiches);
+  // Transformation des fiches en JSON
+  const valeurFiches = JSON.stringify(fiches);
+  // Stockage des informations dans le sessionStorage
+  window.sessionStorage.setItem("fiches", valeurFiches);
 } else {
-    fiches = JSON.parse(fiches);
+  fiches = JSON.parse(fiches);
 }
 
-// Création des balises 
+// Création des balises
 function genererFiches(fiches) {
-    for (let i = 0; i < fiches.length; i++) {
-        const works = fiches[i];
+  for (let i = 0; i < fiches.length; i++) {
+    const works = fiches[i];
 
-        // Récupération de l'élément du DOM qui accueillera les fiches
-        const divGallery = document.querySelector(".gallery");
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const divGallery = document.querySelector(".gallery");
 
-        //création de la balise pour les fiches - balise<figure>
-        const ficheElement = document.createElement("figure");
-        //Création des balises
-        const imageElement = document.createElement("img");
-        imageElement.src = works.imageUrl;
-        const titleElement = document.createElement("figcaption");
-        titleElement.innerText = works.title;
+    //création de la balise pour les fiches - balise<figure>
+    const ficheElement = document.createElement("figure");
+    //Création des balises
+    const imageElement = document.createElement("img");
+    imageElement.src = works.imageUrl;
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = works.title;
 
-        // On rattache la balise article a la div gallery
-        divGallery.appendChild(ficheElement);
+    // On rattache la balise article a la div gallery
+    divGallery.appendChild(ficheElement);
 
-        //Rattachement de nos balises au DOM
-        ficheElement.appendChild(imageElement);
-        ficheElement.appendChild(titleElement);
-
-
-    }
+    //Rattachement de nos balises au DOM
+    ficheElement.appendChild(imageElement);
+    ficheElement.appendChild(titleElement);
+  }
 }
 
 //Création des fiches
@@ -52,44 +51,43 @@ genererFiches(fiches);
 const btnall = document.querySelector(".btnall");
 
 btnall.addEventListener("click", function () {
-    const filtresFiches = fiches.filter(function (filtres) {
-        return filtres.category;
-    })
-    //console.log(filtresFiches);
-    document.querySelector(".gallery").innerHTML = "";
-    genererFiches(filtresFiches);
+  const filtresFiches = fiches.filter(function (filtres) {
+    return filtres.category;
+  });
+
+  document.querySelector(".gallery").innerHTML = "";
+  genererFiches(filtresFiches);
 });
 //Filtres objets
 const btnobjets = document.querySelector(".btnobjets");
 
 btnobjets.addEventListener("click", function () {
-    const filtresFiches = fiches.filter(function (filtres) {
-        return filtres.category.id == 1;
-    })
-    //console.log(filtresFiches);
-    document.querySelector(".gallery").innerHTML = "";
-    genererFiches(filtresFiches);
+  const filtresFiches = fiches.filter(function (filtres) {
+    return filtres.category.id == 1;
+  });
+
+  document.querySelector(".gallery").innerHTML = "";
+  genererFiches(filtresFiches);
 });
 //Filtres appart
 const btnappart = document.querySelector(".btnappart");
 
 btnappart.addEventListener("click", function () {
-    const filtresFiches = fiches.filter(function (filtres) {
-        return filtres.category.id == 2;
-    })
-    //console.log(filtresFiches);
-    document.querySelector(".gallery").innerHTML = "";
-    genererFiches(filtresFiches);
+  const filtresFiches = fiches.filter(function (filtres) {
+    return filtres.category.id == 2;
+  });
+
+  document.querySelector(".gallery").innerHTML = "";
+  genererFiches(filtresFiches);
 });
 //Filtres hotels
 const btnhotels = document.querySelector(".btnhotels");
 
 btnhotels.addEventListener("click", function () {
-    const filtresFiches = fiches.filter(function (filtres) {
-        return filtres.category.id == 3;
-    })
-    //console.log(filtresFiches);
-    document.querySelector(".gallery").innerHTML = "";
-    genererFiches(filtresFiches);
-});
+  const filtresFiches = fiches.filter(function (filtres) {
+    return filtres.category.id == 3;
+  });
 
+  document.querySelector(".gallery").innerHTML = "";
+  genererFiches(filtresFiches);
+});
